2040e149b97ab25eb92671b79d2b6175bd88649d
denishm116 <denishm116@yandex.ru>
2020-07-22 15:12:43 +0300

    Add vkontakte login

--- config/services.php
+++ config/services.php
@@ -37,4 +37,10 @@ return [
 
     ],
 
+    'vkontakte' => [
+        'client_id' => env('VKONTAKTE_CLIENT_ID'),
+        'client_secret' => env('VKONTAKTE_CLIENT_SECRET'),
+        'redirect' => env('VKONTAKTE_REDIRECT_URL')
+    ],
+
 ];
