0677b0a959227d3130c7784572616beafe034021
denishm116 <denishm116@yandex.ru>
2020-07-22 18:40:13 +0300

    Add facebook login

--- config/services.php
+++ config/services.php
@@ -49,4 +49,10 @@ return [
         'redirect' => env('YANDEX_REDIRECT_URL')
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URL')
+    ],
+
 ];
